bb41fd61f18d523a1495be02b00599698e9944e6
pipeb <pipeba1408@gmail.com>
2024-08-28 12:11:17 -0500

    Initial commit

---
+++ src/main/java/com/patrones/parcial1_api/Parcial1ApiApplication.java
@@ -0,0 +1,13 @@
+package com.patrones.parcial1_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial1ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patrones/parcial1_api/Parcial1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrones.parcial1_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
