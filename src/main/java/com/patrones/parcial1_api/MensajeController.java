3e79905b7db587d8d3fb3fa3c76c8fcf50caa056
Felipe <pipeba1408@gmail.com>
2024-08-29 18:12:34 -0500

    Add CrossOrigin

--- src/main/java/com/patrones/parcial1_api/MensajeController.java
+++ src/main/java/com/patrones/parcial1_api/MensajeController.java
@@ -12,16 +12,19 @@ public class MensajeController {
     @Autowired
     private MensajeRepository mensajeRepository;
 
+    @CrossOrigin(origins = {"http://24.199.65.203:3000", "*"})
     @GetMapping("/mensajes")
     public List<Mensaje> getAllMensajes(){
         return mensajeRepository.findAll();
     }
 
+    @CrossOrigin(origins = {"http://24.199.65.203:3000", "*"})
     @PostMapping("/nuevo_mensaje")
     public Mensaje agregarMensaje(@RequestBody Mensaje mensaje){
         return mensajeRepository.save(mensaje);
     }
 
+    @CrossOrigin(origins = {"http://24.199.65.203:3000", "*"})
     @DeleteMapping("/borrar")
     public void borrarMensajes() {
         mensajeRepository.deleteAll();
